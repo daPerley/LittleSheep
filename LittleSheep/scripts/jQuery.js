fd26b72716a225d0d7141fa3e1311c3db9926ab7
Perly Gustafsson <contact@daperley.com>
2016-09-22 23:34:56 +0200

    Mobilanpassad
    Mobilanpassat layouten

---
+++ LittleSheep/scripts/jQuery.js
@@ -0,0 +1,6 @@
+﻿jQuery(document).ready(function () {
+    jQuery('.toggle-nav, header nav a').click(function (e) {
+        jQuery('.toggle-nav').toggleClass('active');
+        jQuery('.active').toggleClass('active');
+    });
+});
